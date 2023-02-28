56a5f3f4b630dd9aeff46ea443c2f11a9334f0d3
MishaHitko <mishagitko1997@gmail.com>
2023-02-28 16:26:17 +0300

    fix bag valid inputs

--- components/validate.js
+++ components/validate.js
@@ -5,69 +5,78 @@ export function validate() {
     }
 
     const nameInput = document.querySelector('input[name="name"]');
+    const passwordInput = document.querySelector('input[name="password"]')
     const ageInput = document.querySelector('input[name="age"]');
     const emailInput = document.querySelector('input[name="email"]');
     const telInput = document.querySelector('input[name="tel"]');
     const bankInput = document.querySelector('input[name="bank"]');
 
-    const inputs = [nameInput, ageInput, emailInput, telInput, bankInput];
+    const inputs = [nameInput, passwordInput, ageInput, emailInput, telInput, bankInput];
 
     const FORM_VALID = {
         name: {
             reqExp: /^[А-Я A-Z]+$/i,
-            input: nameInput,
+            input: name,
             errorMessage: 'Некорректно введено имя!'
+        },
+        password: {
+            reqExp: /^[a-zA-Z0-9!@#$%^&*()_+~`|}{[\]\\:';"<>,.?/]{6,20}$/,
+            errorMessage: 'Некорректно введен пароль!(Пароль должен содержать от 6 до 20 символов)'
         },
         age: {
             reqExp: /^[0-9]{1,2}$/i,
-            input: ageInput,
             errorMessage: 'Некорректно введен возраст!'
         },
         email: {
             reqExp: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
-            input: emailInput,
             errorMessage: 'Некорректно введен имейл!'
         },
         tel: {
             reqExp: /^[0-9]{10,13}$/i,
-            input: telInput,
             errorMessage: 'Некорректно введен номер телефона!'
         },
         bank: {
             reqExp: /^[0-9]{16}$/i,
-            input: bankInput,
             errorMessage: 'Некорректно введен номер банковской карты!'
         }
     };
+    const isFormValid = (name, value, input)=> {
+        const {reqExp, errorMessage} = FORM_VALID[name];
 
-    const isFormValid = formData => {
-
-        for (let key in formData) {
-            const {reqExp, input, errorMessage} = formData[key];
-
-            const buttonSave = document.querySelector('.button_form_save')
-            const errorValid = document.querySelector('.text-error');
-
-            if (!reqExp.test(input.value)) {
-                if(!errorValid) {
-                    const errorValid = document.createElement('p');
-                    errorValid.classList.add('text-error');
-                    errorValid.innerText = errorMessage;
-                    const blockName = input.closest('div');
-                    blockName.append(errorValid);
-                }
-                buttonSave.disabled = true;
-            } else {
-                if(errorValid) {
-                    errorValid.remove();
-                }
-                buttonSave.disabled = false;
-            }
+        if (!reqExp.test(value)) {
+            showError(errorMessage, input);
+            blockButtonSave()
+        } else {
+            removeError(input);
+            blockButtonSave()
         }
     }
     inputs.forEach((input) => {
-        input.onchange = () => {
-            isFormValid(FORM_VALID);
+        input.onchange = ({target}) => {
+            isFormValid(target.name, target.value, target);
         }
     })
+}
+
+function showError(errorMessage, input) {
+    const blockName = input.closest('div');
+    const errorElem = blockName.querySelector('.text-error');
+    if(!errorElem) {
+        const errorValid = document.createElement('p');
+        errorValid.classList.add('text-error');
+        errorValid.innerText = errorMessage;
+        blockName.append(errorValid);
+    }
+}
+function removeError(input) {
+    const blockName = input.closest('div');
+    const errorValid = blockName.querySelector('.text-error');
+    if(errorValid) {
+        errorValid.remove();
+    }
+}
+function blockButtonSave() {
+    const errorValid = document.querySelector('.text-error');
+    const buttonSave = document.querySelector('.button_form_save');
+    errorValid ? buttonSave.disabled = true : buttonSave.disabled = false;
 }
